b87e4fcd7f2a49f4e1051010ea16d1a745ab4ac0
Christopher Lundblade <chris.lundblade@gmail.com>
2021-07-15 14:37:39 -0400

    completed for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -7,3 +7,36 @@ function showMultiplicationTable(input){
 }
 
 //my name jeff
+
+function randomNumbersOddEven() {
+    for (let i = 1; i <= 20; i++) {
+        let newRand = Math.floor(Math.random() * 180) + 20;
+        if (newRand % 2 === 0) {
+            console.log(newRand + " is even.");
+        } else {
+            console.log(newRand + " is odd.");
+        }
+    }
+}
+
+function stringConcatLoop(){
+        for (let i=1; i <=9; i++){
+            let counter = 0;
+            let string = "";
+            //let newArray = [];
+            while(counter <i){
+                string += i;
+                counter++;
+            }
+            //newArray.forEach(element => string += element);
+            console.log(string);
+        }
+}
+
+function countDownByFive(){
+    for (let i=100; i > 0; i=i-5){
+        console.log(i);
+    }
+}
+
+
