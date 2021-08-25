528710f6d40a10251b16b8a1bd225d2bc7d92a90
Christopher Lundblade <chris.lundblade@gmail.com>
2021-08-25 14:10:34 -0500

    completed narcissistic number challenge in amateur_coding_challenge.js

--- js/amateur_coding_challenge.js
+++ js/amateur_coding_challenge.js
@@ -13,3 +13,54 @@ function sequenceSum(start, finish, step){
     return sum;
 }
 
+
+// A narcissistic number is a non-negative integer (n) with m digits where the sum of all the individual digits raised to the power m is equal to n.
+//
+//     For example:
+//
+//     If n is 153, then m (the number of digits) is 3 and:
+//
+//     1**3 + 5**3 + 3**3 = 1 + 125 + 27 = 153
+//
+// So, 153 is a narcissistic number.
+//
+//     Objective: Write a script to generate and output the first 25 narcissistic integers.
+const getDigitsArray = theNumber =>{
+    let digitsArray = [];
+    let numAsString = theNumber.toString();
+    for (let i = 0; i < numAsString.length; i++){
+        digitsArray.push(numAsString.charAt(i));
+
+    }
+    //console.log(digitsArray);
+    return digitsArray;
+}
+
+function isNarcNum(arrayOfDigits, theNumberN){
+    let digitPowerSum = 0;
+    arrayOfDigits.forEach(digit => {
+        digitPowerSum += parseFloat(digit) ** arrayOfDigits.length;
+        //console.log(parseFloat(digit) ** arrayOfDigits.length);
+    });
+    if (theNumberN === digitPowerSum){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+const getNarcNumbers = () => {
+    let narcNumberArray = []
+    let i = 1;
+    while (narcNumberArray.length !== 25){
+        if(isNarcNum(getDigitsArray(i),i)){
+            narcNumberArray.push(i);
+            console.log(`${i} is narcissistic. Array length ${narcNumberArray.length}`);
+        }
+        i++;
+    }
+    return narcNumberArray;
+}
+
+getNarcNumbers();
+//console.log(isNarcNum(getDigitsArray(153),153));
